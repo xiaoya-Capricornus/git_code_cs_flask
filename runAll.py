34e6fa0de95bb79a9e799876d86a3746bccc710e
xiaoya <xyyy1227@163.com>
2021-07-30 17:03:11 +0800

    add thread

--- runAll.py
+++ runAll.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import os
+
+# from pytest_html.plugin import HTMLReport
+# import HTMLTestReport
 import Common.HTMLTestRunner as HTMLTestRunner
 import getpathInfo
 import getUrlParams
@@ -12,6 +15,9 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 # import pythoncom
 import unittest
 import Common.Log
+import threading
+from Common.tomorrow import threads
+import time
 
 LOG = Common.Log.logger
 SEND_EMAIL = SendEmail(
@@ -54,6 +60,7 @@ class AllTest():
         self.set_case_list()  # 通过set_case_list()拿到caselist元素组
         test_suite = unittest.TestSuite()
         suite_module = []
+        threads = []
         for case in self.caseList:  # 从caselist元素组中循环取出case
             case_name = case.split("/")[-1]  # 通过split函数来将aaa/bbb分割字符串，-1取后面，0取前面
             print(case_name + ".py")  # 打印出取出来的名称
@@ -66,27 +73,54 @@ class AllTest():
             for suite in suite_module:  # 如果存在，循环取出元素组内容，命名为suite
                 for test_name in suite:  # 从discover中取出test_name，使用addTest添加到测试集
                     test_suite.addTest(test_name)
+                # for test_name in suite:  # 从discover中取出test_name，使用addTest添加到测试集
+                #     # 多线程
+                #     print("multi_thread begin")
+                #     threads.append(threading.Thread(target=test_suite.addTest, args=(test_name)))
+                #
+                # for thread in threads:
+                #     thread.start()
+                #
+                # for thread in threads:
+                #     thread.join()
+                # print("multi_thread end")
         else:
             print('else:')
             return None
+        print(test_suite)
         return test_suite  # 返回测试集
 
-    def run(self, SEND_MAIL=None):
+    @threads(10) #多线程
+    def run_case(self, suit):
+        now = time.strftime('%Y-%m-%d %H-%M-%S')
+        report_file = os.path.join(REPORT_PATH, now + 'report.html')
+        fp = open(report_file, 'wb')  # 打开result/report.html测试报告文件，如果不存在就创建
+        # 调用HTMLTestRunner
+        runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title='Test Report',
+                                               description='Test Description')
+        # runner = HTMLReport.TestRunner(
+        #     title="XXX项目测试报告",
+        #     description="测试人员：CesareCheung",
+        #     report_file_name=f"testreport",
+        #     thread_count=50
+        # )
+        runner.run(suit)
+
+    def run(self, suit, SEND_MAIL=None):
         """
         run test
         :return:
         """
+        t1 = time.time()
         try:
-            suit = self.set_case_suite()  # 调用set_case_suite获取test_suite
             print('try')
             print(str(suit))
             if suit is not None:  # 判断test_suite是否为空
                 print('if-suit')
-                fp = open(RESULT_PATH, 'wb')  # 打开result/20181108/report.html测试报告文件，如果不存在就创建
-                # 调用HTMLTestRunner
-                runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title='Test Report',
-                                                       description='Test Description')
-                runner.run(suit)
+                self.run_case(suit)
+                t2 = time.time()
+                print('end:%s' % time.ctime())
+                print('总共花费时间:%s' % (t2 - t1))
             else:
                 print("Have no case to test.")
         except Exception as ex:
@@ -96,12 +130,14 @@ class AllTest():
         finally:
             print("*********TEST END*********")
             LOG.info("*********TEST END*********")
-            fp.close()
+            # fp.close()
         # 判断邮件发送的开关
         if ON_OFF == 'on':
             SEND_EMAIL.send_email()
         else:
             print("邮件发送开关配置关闭，请打开开关后可正常自动发送测试报告")
+
+
 
 # pythoncom.CoInitialize()
 # scheduler = BlockingScheduler()
@@ -110,7 +146,11 @@ class AllTest():
 
 
 if __name__ == '__main__':
-    AllTest().run()
+    # 调用set_case_suite获取test_suite
+    cases = AllTest()
+    for suit in cases.set_case_suite():
+        print(suit, type(suit))
+        AllTest().run(suit)
 
 
 
